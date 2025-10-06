e69fea1eb6ef7751858fe007fdfabd7c2624ceee
coderinsightmaven <lmarincik@proton.me>
2025-10-06 15:04:37 -0600

    Update Next.js configuration to allow all image hostnames and enhance Projects component with a new project entry, improving content and structure.

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -13,9 +13,20 @@ const posts = [
       date: 'Sept 25th, 2025',
       datetime: '2025-09-25',
     },
+    {
+        id: 2,
+        title: 'Match Point Systems Tennis Api',
+        href: 'https://github.com/Coderinsightmaven/mps-tennisapi',
+        description:
+          'Live Data Integration, for Ioncourt a scoring application for ITA Tennis Tournaments built with Tauri v2, Vite, Tailwind, Typescript, and Rust',
+        imageUrl:
+          'https://teachyourkidscode.com/wp-content/uploads/2022/02/best-coding-language-for-games.jpg',
+        date: 'Sept 15th, 2025',
+        datetime: '2025-09-25',
+      },
   ]
   
-  export default function Example() {
+  export default function Projects() {
     return (
       <div className="bg-white py-24 sm:py-32 dark:bg-gray-900">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
@@ -24,7 +35,7 @@ const posts = [
               Past Projects
             </h2>
             <p className="mt-2 text-lg/8 text-gray-600 dark:text-gray-400">
-                A list of my past projects.
+                    
             </p>
           </div>
           <div className="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
