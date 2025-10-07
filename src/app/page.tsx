8c37712920958dd705a1e4680a829cf2c1d61efa
coderinsightmaven <lmarincik@proton.me>
2025-10-07 11:48:20 -0600

    Add Footer component to Home page and implement social media links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@
 
 import HeroSection from "@/components/HeroSection";
 import Projects from "@/components/projects";
+import Footer from "@/components/footer";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <Projects />
-      </>
+      <Footer />
+    </>
   );
 }
